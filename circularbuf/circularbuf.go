a591b087314e00bd6f152fae27565027d0e05610
Bartosz Lenart <lenart.consulting@gmail.com>
2023-03-07 10:08:43 +0100

    circular buffer remove and remove seek

--- circularbuf/circularbuf.go
+++ circularbuf/circularbuf.go
@@ -38,6 +38,57 @@ func (b *Buffer[T]) Add(value T) {
 	b.inner = append(b.inner[:b.cur], append([]T{value}, b.inner[b.cur+1:]...)...)
 }
 
+// RemoveSeek removes value at given cursor position.
+// Returns value at removed cursor or error otherwise.
+func (b *Buffer[T]) RemoveSeek(cur int) (T, error) {
+	if cur < 0 || cur >= len(b.inner) {
+		var t T
+		return t, errors.New("cursor out of buffer range")
+	}
+	t := b.inner[cur]
+	switch {
+	case cur == 0:
+		b.inner = b.inner[1:]
+	case cur == len(b.inner):
+		b.inner = b.inner[:len(b.inner)-1]
+	default:
+		b.inner = append(b.inner[:b.cur], b.inner[b.cur+1:]...)
+	}
+
+
+	if cur < b.cur {
+		b.cur--
+	}
+	if b.cur < 0 {
+		b.cur = len(b.inner) - 1
+	}
+	if b.cur >= len(b.inner) {
+		b.cur = 0
+	}
+	
+	return t, nil
+}
+
+// Remove removes value from current cursor position.
+// Returns removed value.
+func (b *Buffer[T]) Remove() T {
+	t := b.inner[b.cur]
+	switch {
+	case b.cur == 0:
+		b.inner = b.inner[1:]
+	case b.cur == len(b.inner):
+		b.inner = b.inner[:len(b.inner)-1]
+	default:
+		b.inner = append(b.inner[:b.cur], b.inner[b.cur+1:]...)
+	}
+	if b.cur == len(b.inner) {
+		b.cur = 0
+	}
+
+	return t
+
+}
+
 // Next sets cursor to the next position and returns value at this position.
 func (b *Buffer[T]) Next() (T, error) {
 	if b.cur < 0 {
